8a2a928fa6f640f6fb3c07809a89dc7802e766b8
HPhucJ2007 <yourgitemail@example.com>
2023-07-30 21:56:05 +0700

    pagination with pagination service provider

--- resources/views/listings/index.blade.php
+++ resources/views/listings/index.blade.php
@@ -11,5 +11,10 @@
                 <x-listing-card :listing="$listing" />
             @endforeach
         @endif
+
+    </div>
+    
+    <div class="mt-6 p-4">
+        {{$listings->links()}}
     </div>
 </x-layout>
