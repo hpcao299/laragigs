33eb56db8f7ea46ada87a171014737bee3cee23b
HPhucJ2007 <yourgitemail@example.com>
2023-07-30 14:33:07 +0700

    database base migration and seeder

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -2,32 +2,10 @@
 
 namespace App\Models;
 
-class Listing
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Listing extends Model
 {
-    public static function all()
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Item One',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis vel iste magni reiciendis ab fugiat vitae ipsam inventore, nulla maiores. Ea ut libero perspiciatis molestiae impedit nobis delectus assumenda.'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Item Two',
-                'description' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus quis vel iste magni reiciendis ab fugiat vitae ipsam inventore, nulla maiores. Ea ut libero perspiciatis molestiae impedit nobis delectus assumenda.'
-            ],
-        ];
-    }
-
-    public static function find(int $id)
-    {
-        $listings = self::all();
-
-        foreach ($listings as $listing) {
-            if ($listing['id'] == $id) {
-                return $listing;
-            }
-        }
-    }
+    use HasFactory;
 }
